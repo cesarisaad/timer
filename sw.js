120bdce2989c613c73898507a06d8e575efbd467
cesarisaad <140536181+cesarisaad@users.noreply.github.com>
2026-09-16 21:26:25 -0400

    Update sw.js
    version 6.5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cambia la versión del caché a v4.1 para forzar la actualización
-const CACHE_NAME = 'tabata-timer-v6.4'; 
+const CACHE_NAME = 'tabata-timer-v6.5'; 
 
 const ASSETS_TO_CACHE = [
   './',
